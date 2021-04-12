98a32590091790479b861e262eeea10f9703367a
JesusIsBooky <marshallh2016@gmail.com>
2021-04-12 01:53:54 +0100

    Initinal commit

---
+++ commands/clear.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'clear',
+    description: "Clear messages!",
+   async  execute(message, args) {
+        if (!args[0]) return message.reply("Please enter the amount of messages to clear!");
+ 
+        if(isNaN(args[0])) return message.reply("Please type a real number!");
+ 
+        if(args[0] > 100) return message.reply("You can't remove more than 100 messages!");
+        
+        if(args[0] < 1) return message.reply("You have to delete at least one message!");
+ 
+        await message.channel.messages.fetch({ limit: args[0]}).then(messages =>{
+            message.channel.bulkDelete(messages)
+    });
+ 
+ }
+}  
